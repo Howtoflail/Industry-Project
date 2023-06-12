78f14eb18ff8c93396d0b7d6411d14647440ac05
markassim <markassimkfont@gmail.com>
2023-06-12 21:54:48 +0200

    add user objects

---
+++ KindRegardsUnity/Assets/User/Scripts/PetInfo.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Firebase.Firestore;
+
+[StorageCollection("Pets")]
+[FirestoreData]
+public class PetInfo : MonoBehaviour
+{
+	public UnityEvent onLoaded;
+	private UserInfo userInfo;
+
+	public void LoadInfo()
+	{
+		userInfo = GameObject.FindWithTag("UserInfo").GetComponent<UserInfo>();
+		id = userInfo.uniqueId;
+		
+		var mgr = GameObject.FindWithTag("FirestoreManager").GetComponent<FirestoreManager>();
+		
+		mgr.GetObject(this, result => {
+			if (result != null)
+			{
+				FirestoreDataTransferer.TransferTo(result as DocumentSnapshot, this);
+				print(petName);
+			}
+			
+			onLoaded.Invoke();
+		});
+	}
+	
+	[FirestoreDocumentId]
+	public string id
+	{
+		get;
+		private set;
+	}
+
+	[FirestoreProperty]
+	public string petName
+	{
+		get;
+		set;
+	}
+
+	[FirestoreProperty]
+	public string petType
+	{
+		get;
+		set;
+	}
+
+	[FirestoreProperty]
+	public string petColor
+	{
+		get;
+		set;
+	}
+}
