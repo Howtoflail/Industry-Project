030a9bbfd32a99582eb4324d9045998636f7f977
RowanDongen <rowandongen@gmail.com>
2023-06-16 11:46:13 +0200

    BRUH
    merge main into flappy bird and keep pet type picker with no petInfo

--- KindRegardsUnity/Assets/Scripts/UI/PetTypePicker.cs
+++ KindRegardsUnity/Assets/Scripts/UI/PetTypePicker.cs
@@ -2,54 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System;
 
 public class PetTypePicker : MonoBehaviour
 {
     // Start is called before the first frame update
-    
-    
+
     [SerializeField]
     private List<AudioClip> animalAudioClips;
+
     [SerializeField]
     private PetController petController;
+
     [SerializeField]
     private Text typeText;
     private AudioSource audioSource;
+    [SerializeField]
+    private int currentSelectedType;
 
     PetInfo petInfo;
+
     void Start()
     {
+        currentSelectedType = 0;
         petInfo = GameObject.FindWithTag("PetInfo").GetComponent<PetInfo>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     public void UpdateTypeText()
     {
-        switch (Enum.Parse(typeof(PetStateEnum), petInfo.petType))
+        switch (currentSelectedType)
         {
-            case PetStateEnum.Rabbit:
-            typeText.text = "Rabbit";
-            break;
-            case PetStateEnum.Owl:
-            typeText.text = "Owl";
-            break;
-            case PetStateEnum.Cat:
-            typeText.text = "Cat";
-            break;
-            case PetStateEnum.Dog:
-            typeText.text = "Dog";
-            break;
+            case 0:
+                typeText.text = PetStateEnum.Rabbit.ToString();
+                break;
+            case 1:
+                typeText.text = PetStateEnum.Owl.ToString();
+                break;
+            case 2:
+                typeText.text = PetStateEnum.Cat.ToString();
+                break;
+            case 3:
+                typeText.text = PetStateEnum.Dog.ToString();
+                break;
             default:
-            typeText.text = "-";
-            break;
+                break;
         }
     }
+
+    public void Next()
+    {
+        currentSelectedType++;
+        if (currentSelectedType > 3)
+        {
+            currentSelectedType = 0;
+        }
+        return;
+    }
+
+    public void Previous()
+    {
+        currentSelectedType--;
+        if (currentSelectedType < 0)
+        {
+            currentSelectedType = 3;
+        }
+        return;
+    }
+
     // public void PlayTypeSound()
     // {
     //     switch (Enum.Parse(typeof(PetStateEnum), petInfo.petType))
