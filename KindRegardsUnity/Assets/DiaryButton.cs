c46782a5000d9148e49525938fb3c93cec3b0fad
KD <demiebroerswerk@gmail.com>
2023-06-21 12:53:25 +0200

    Animiation added

--- KindRegardsUnity/Assets/DiaryButton.cs
+++ KindRegardsUnity/Assets/DiaryButton.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class DiaryButton : MonoBehaviour
 {
+    [SerializeField]
+    private Animator diaryNotifcation;
     public Button button;
     // Start is called before the first frame update
     void Start()
     {
         Button btn = button.GetComponent<Button>();
         btn.onClick.AddListener(DisableButtonOnClick);
+        StartCoroutine(PlayNudge());
+        Invoke("PlayNudge", 100);
     }
 
     // Update is called once per frame
@@ -22,5 +26,10 @@ public class DiaryButton : MonoBehaviour
     void DisableButtonOnClick()
     {
         button.gameObject.SetActive(false);
+    }
+    private void PlayNudge()
+    {
+
+        diaryNotifcation.Play("LittleNudge");
     }
 }
